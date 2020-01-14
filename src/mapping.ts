ddadcaa09d528f32964d6cce54fd445a66b53e78
Aleks Gnatovskyi <aleks.work2222@gmail.com>
2020-01-14 12:32:42 +0200

    debt: proposals and pledges handlers

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts";
+import { BigInt, crypto, ByteArray } from "@graphprotocol/graph-ts";
 import { Status } from "../generated/FundsModule/FundsModule";
 import {
   Deposit,
@@ -12,7 +12,8 @@ import {
   Repay,
   UnlockedPledgeWithdraw
 } from "../generated/LoanModule/LoanModule";
-import { Staker, User, Debt, Pool } from "../generated/schema";
+import { User, Debt, Pool, Pledge } from "../generated/schema";
+import {concat} from "./utils";
 
 export function handleStatus(event: Status): void {
   let pool = new Pool(new Date().getTime().toString());
@@ -22,6 +23,14 @@ export function handleStatus(event: Status): void {
   pool.pExitPrice = event.params.pExitPrice;
 
   pool.save();
+
+  //refresh latest
+  let latest_pool = Pool.load("latest");
+  if (latest_pool == null) {
+    latest_pool = new Pool("latest");
+  }
+  latest_pool = pool;
+  latest_pool.save();
 }
 
 export function handleDeposit(event: Deposit): void {
@@ -38,24 +47,73 @@ export function handleDeposit(event: Deposit): void {
   user.address = event.params.sender;
   user.lBalance = user.lBalance.plus(event.params.lAmount);
   user.pBalance = user.lBalance.plus(event.params.pAmount);
-  user.save()
+  user.save();
 }
 
 export function handleWithdraw(event: Withdraw): void {
   let user = User.load(event.params.sender.toHex());
+  if (user == null) return;
 
   user.address = event.params.sender;
   user.lBalance = user.lBalance.minus(event.params.lAmountTotal);
   user.pBalance = user.lBalance.minus(event.params.pAmount);
-  user.save()
+  user.save();
 
-  // let pool = new Pool(new Date().getTime().toString());
-  // pool.lBalance = event.params.lBalance;
-  // pool.lDebt = event.params.lDebt;
-  // pool.pEnterPrice = event.params.pEnterPrice;
-  // pool.pExitPrice = event.params.pExitPrice;
+  let pool = Pool.load("latest");
+  pool.lBalance = pool.lBalance.minus(event.params.lAmountTotal);
+  pool.save();
+}
 
-  // pool.save();
+export function handleDebtPoposalCreated(event: DebtProposalCreated): void {
+  let proposal = new Debt(event.params.proposal.toHex());
+  proposal.borrower = event.params.sender;
+  proposal.total = event.params.lAmount;
+  proposal.repayed = BigInt.fromI32(0);
+  proposal.pledges = [];
+  proposal.status = "PROPOSED";
+  proposal.save();
+}
+
+export function handleDebtPoposalExecuted(event: DebtProposalExecuted): void {
+  let proposal = Debt.load(event.params.proposal.toHex());
+  proposal.status = "EXECUTED";
+  proposal.save();
+}
+
+export function handlePledgeAdded(event: PledgeAdded): void {
+  let proposal = new Debt(event.params.proposal.toHex());
+
+  let hash = crypto.keccak256(concat(event.params.sender, event.params.borrower)).toHexString()
+  let pledge = new Pledge(hash);
+  pledge.pledger = event.params.sender;
+  pledge.lAmount = event.params.lAmount;
+  pledge.pAmount = event.params.pAmount;
+  pledge.save();
+
+  proposal.pledges.push(pledge.toString());
+  proposal.save()
+}
+
+export function handlePledgeWithdrawn(event: PledgeWithdrawn): void {
+  let proposal = new Debt(event.params.proposal.toHex());
+  let hash = crypto.keccak256(concat(event.params.sender, event.params.borrower)).toHexString()
+  let new_arr = proposal.pledges.filter(p => !p.includes(hash));
+
+  let pledge = Pledge.load(hash);
+  pledge.pledger = event.params.sender;
+  pledge.lAmount = event.params.lAmount;
+  pledge.pAmount = event.params.pAmount;
+  pledge.save();
+
+  proposal.pledges = new_arr;
+  proposal.save()
 }
 
 
+export function handleRepay(event: Repay): void {
+
+}
+
+export function handleUnlockedPledgeWithdraw(
+  event: UnlockedPledgeWithdraw
+): void {}
---
+++ src/utils.ts
@@ -0,0 +1,30 @@
+import { ByteArray } from "@graphprotocol/graph-ts";
+
+export function concat(a: ByteArray, b: ByteArray): ByteArray {
+  let out = new Uint8Array(a.length + b.length);
+  for (let i = 0; i < a.length; i++) {
+    out[i] = a[i];
+  }
+  for (let j = 0; j < b.length; j++) {
+    out[a.length + j] = b[j];
+  }
+  return out as ByteArray;
+}
+
+export function concat_array(...args: [ByteArray]): ByteArray {
+  let reducer = (acc, cur) => acc + cur.length;
+  let len = args.reduce(reducer, 0);
+
+  let out = new Uint8Array(len);
+  let offset = 0;
+  
+  for (let j = 0; j < args.length; j++) {
+    let a = args[j];
+    for (let i = 0; i < a.length; i++) {
+        out[offset + i] = a[i];
+    }
+    offset += a.length;
+  }
+  
+  return out as ByteArray;
+}
