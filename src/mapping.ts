c30d87e1de3c84e3089f3e86f5fbd22ba7956bcc
Aleks Gnatovskyi <aleks.work2222@gmail.com>
2020-01-15 15:48:22 +0200

    history: change user history

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,5 +1,6 @@
-import { BigInt, Bytes, crypto } from "@graphprotocol/graph-ts";
-import { Status } from "../generated/FundsModule/FundsModule";
+import { BigInt, Bytes, crypto, Address } from "@graphprotocol/graph-ts";
+import { Status, FundsModule } from "../generated/FundsModule/FundsModule";
+import { PToken, Transfer } from "../generated/PToken/PToken";
 import {
   Deposit,
   Withdraw
@@ -26,61 +27,74 @@ export function handleStatus(event: Status): void {
   pool.save();
 
   //refresh latest
-  let latest_pool = Pool.load("latest");
-  if (latest_pool == null) {
-    latest_pool = new Pool("latest");
-    latest_pool.lBalance = BigInt.fromI32(0);
-    latest_pool.lDebt = BigInt.fromI32(0);
-    latest_pool.pEnterPrice = BigInt.fromI32(0);
-    latest_pool.pExitPrice = BigInt.fromI32(0);
-  }
-  latest_pool = pool;
+  let latest_pool = get_latest_pool();
+  latest_pool.lBalance = pool.lBalance;
+  latest_pool.lDebt = pool.lDebt;
+  latest_pool.pEnterPrice = pool.pEnterPrice;
+  latest_pool.pExitPrice = pool.pExitPrice;
   latest_pool.save();
+
+  // TODO: add new balance in history of all users once a day
+  // TEST: will only work if timestamp returned in ms
+  const DAY = 86400000;
+  let today = event.block.timestamp.div(BigInt.fromI32(DAY));
+  let balance = Balance.load(today.toHex());
+  
+  // once a day
+  if(balance == null){  
+    let yesterday = Balance.load(today.minus(BigInt.fromI32(1)).toHex());
+    balance = new Balance(today.toHex());
+    balance.user = "daily";
+    balance.lBalance = yesterday.lBalance;
+    balance.pBalance = yesterday.pBalance;
+
+    pool.users.forEach(address => {
+      let user = User.load(address);
+
+      // get current liquid from current PTK
+      let Funds_mod = FundsModule.bind(event.address);
+      let result = Funds_mod.calculatePoolExitInverse(user.pBalance)
+      let user_lBalance = result.value1;
+
+      // add balance record
+      let balance = new Balance(event.block.timestamp.toHex());
+      balance.pBalance = user.pBalance;
+      balance.lBalance = user_lBalance;
+      balance.user = user.id;
+      balance.save();
+
+      //push record to user history
+      user.history.push(balance.id);
+      user.save();
+    })
+  }
+}
+
+export function handleTransfer(event: Transfer): void {
+  let from = get_user(event.params.from);
+  let to = get_user(event.params.to);
+  let pool = get_latest_pool();
+  if(!pool.users.includes(from.id)){
+    pool.users.push(from.id);
+    pool.save()  
+  }
+  if(!pool.users.includes(to.id)){
+    pool.users.push(to.id);
+    pool.save()  
+  }
+  from.pBalance = from.pBalance.minus(event.params.value);
+  to.pBalance = to.pBalance.plus(event.params.value);
+  from.save();
+  to.save();
 }
 
 export function handleDeposit(event: Deposit): void {
-  let user = User.load(event.params.sender.toHex());
-
-  if (user == null) {
-    user = new User(event.params.sender.toHex());
-    user.lBalance = BigInt.fromI32(0);
-    user.pBalance = BigInt.fromI32(0);
-    user.locked = BigInt.fromI32(0);
-    user.credit = BigInt.fromI32(0);
+  let user = get_user(event.params.sender);
+  let pool = get_latest_pool();
+  if(!pool.users.includes(user.id)){
+    pool.users.push(user.id);
+    pool.save()  
   }
-
-  let balance = new Balance(event.block.timestamp.toHex());
-  let new_l_balance = user.lBalance.plus(event.params.lAmount);
-  let new_p_balance = user.pBalance.plus(event.params.pAmount);
-
-  balance.lBalance = new_l_balance;
-  balance.pBalance = new_p_balance;
-  balance.user = user.id;
-  balance.save();
-
-  user.lBalance = new_l_balance;
-  user.pBalance = new_p_balance;
-  user.history.push(balance.id);
-  user.save();
-}
-
-export function handleWithdraw(event: Withdraw): void {
-  let user = User.load(event.params.sender.toHex());
-  if (user == null) return;
-
-  let balance = new Balance(event.block.timestamp.toHex());
-  let new_l_balance = user.lBalance.minus(event.params.lAmountTotal);
-  let new_p_balance = user.pBalance.minus(event.params.pAmount);
-
-  balance.lBalance = new_l_balance;
-  balance.pBalance = new_p_balance;
-  balance.user = user.id;
-  balance.save();
-
-  user.lBalance = new_l_balance;
-  user.pBalance = new_p_balance;
-  user.history.push(balance.id);
-  user.save();
 }
 
 export function handleDebtPoposalCreated(event: DebtProposalCreated): void {
@@ -176,12 +190,24 @@ function get_user(address: Bytes): User {
   let user = User.load(address.toHex());
   if (user == null) {
     user = new User(address.toHex());
-    user.lBalance = BigInt.fromI32(0);
     user.pBalance = BigInt.fromI32(0);
     user.locked = BigInt.fromI32(0);
     user.credit = BigInt.fromI32(0);
   }
   return user as User;
+}
+
+function get_latest_pool(): Pool {
+  let latest_pool = Pool.load("latest");
+  if (latest_pool == null) {
+    latest_pool = new Pool("latest");
+    latest_pool.lBalance = BigInt.fromI32(0);
+    latest_pool.lDebt = BigInt.fromI32(0);
+    latest_pool.pEnterPrice = BigInt.fromI32(0);
+    latest_pool.pExitPrice = BigInt.fromI32(0);
+    latest_pool.users = [];
+  }
+  return latest_pool as Pool;
 }
 
 function filter_pledges(p: Debt, hashed: string): Array<string> {
